21f703358c2558b6215c72196e68d56ea26917e5
wigor <wigor21@hotmail.com>
2024-03-03 10:34:36 -0300

    feat: Created menu component

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,23 @@
+export const Menu = () => {
+  return (
+    <nav>
+      <ul className="flex items-center gap-7 text-custom-gray-light">
+        <li>
+          <a href="#">Collections</a>
+        </li>
+        <li>
+          <a href="#">Men</a>
+        </li>
+        <li>
+          <a href="#">Women</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
